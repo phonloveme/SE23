393a9728596ec70df29683323ba2f17ab7a1b811
phonloveme <kirill24550@outlook.com>
2025-02-25 16:24:29 +0500

    done

--- 01_stack_queue/stack.js
+++ 01_stack_queue/stack.js
@@ -7,25 +7,26 @@ class Stack {
         console.log(this.array);
     }
 
-    push = (value) => { // добавить на вершину
-        if (this.array.length === 0) {
-            console.log('Стек пуст!');
-            return null;
-        }
+    push = (...values) => { // добавить на вершину
+        this.array.push(...values);
     }
 
     pop = () => { // снять с вершины и вернуть last
-        
+        return this.array.length === 0 ? console.log('Стек пуст!') : this.array.pop();
     }
 
     peek = () => { // вернуть с вершины, не меняя стек
-        
+        return this.array.length === 0 ? console.log('Стек пуст!') : this.array[this.array.length - 1];
     }
 
     size = () => { // вернуть размер
-        
+        return this.array.length;
     }
 }
 
-const stack = new Stack()
-stack.print()
+const stack = new Stack();
+stack.push(10, 20, 30);
+stack.print(); // Output: [10, 20, 30]
+console.log(stack.pop()); // Output: 30
+console.log(stack.peek()); // Output: 20
+console.log(stack.size()); // Output: 2
