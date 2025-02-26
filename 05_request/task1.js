e444e5d6018b684aa5de449dea15db4651cea58c
phonloveme <94694174+phonloveme@users.noreply.github.com>
2025-02-26 12:57:17 +0500

    Update task1.js

--- 05_request/task1.js
+++ 05_request/task1.js
@@ -4,7 +4,7 @@ const url = 'https://pcoding.ru/txt/labrab04-1.txt';
 request(url, (error, response, body) => {
     // формируем список чисел из тела ответа сервера
     const numbers = body.split(/\s+/).map(Number);
-    // фильтруем только двухзачные числа и сразу ищем максимальное
+    // фильтруем только двухзначные числа и сразу ищем максимальное
     const maxTwoDigitNumbers = Math.max(...(numbers.filter(num => num >= 10 && num < 100)));
     console.log(`Максимальное двухзначное число: ${maxTwoDigitNumbers}`);
 }) 
